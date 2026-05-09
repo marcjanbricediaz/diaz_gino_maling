64542c859afb8230d0b0dc438e058abcd6c345ee
marc jan bryce diaz <diazmarcbrice@gmail.com>
2026-05-09 18:02:43 +0800

    ikaw ubra sang part 4 to 6

---
+++ bryce_gino_maling/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bryce_gino_maling project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bryce_gino_maling.settings')
+
+application = get_asgi_application()
---
+++ bryce_gino_maling/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bryce_gino_maling project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bryce_gino_maling.settings')
+
+application = get_wsgi_application()
