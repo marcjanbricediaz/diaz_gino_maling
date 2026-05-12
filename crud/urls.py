5586e1dbae6d05bfb7b7fea8aafc655f9ae78b01
marc jan bryce diaz <diazmarcbrice@gmail.com>
2026-05-12 16:15:07 +0800

    second update

--- crud/urls.py
+++ crud/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index),
-    path('testing', views.test)
+      path('gender/list', views.gender_list),
+      path('gender/add', views.add_gender),
+      path('gender/edit/<int:genderId>', views.edit_gender),
+      path('gender/delete/<int:genderId>', views.delete_gender)
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,7 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from .models import Genders
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello world')
-def test(request):
-    return render(request, 'layout/base.html')
+
+
+def gender_list(request):
+    try:
+        genders = Genders.objects.all()
+        data = {
+            'genders': genders
+        }
+        return render(request, 'gender/GenderList.html', data)
+    except Exception as e:
+        return HttpResponse(f'Error occured during load genders {e}')
+
+
+def add_gender(request):
+    try:
+        if request.method == 'POST':
+            gender = request.POST.get('gender')
+            Genders.objects.create(gender=gender).save()
+            messages.success(request, 'Gender Added Successfully!')
+            return redirect('/gender/list')
+        else:
+            return render(request, 'gender/AddGender.html')
+    except Exception as e:
+        return HttpResponse(f'Error occured during the add Gender: {e}')
+    
+def edit_gender(request, genderId):
+    try:
+        if request.method == 'POST':
+            genderObj =  Genders.objects.get(pk=genderId)
+
+            gender = request.POST.get('gender')
+
+            genderObj.gender = gender
+            genderObj.save()
+
+            data = {
+                'gender': genderObj
+            }
+ 
+            messages.success(request, 'Gender updated Succesfully!')
+            return render (request, 'gender/EditGender.html', data) 
+        else:    
+            genderObj =  Genders.objects.get(pk=genderId)
+
+            data = {
+                'gender': genderObj
+            } 
+
+            return render(request,'gender/EditGender.html', data)
+      
+    except  Exception as e:
+        return HttpResponse(f'Error occurred during edit gender: {e}')    
+    
+def delete_gender(request, genderId):
+    try:
+        if request.method == 'POST':
+            genderObj = Genders.objects.get(pk=genderId)
+            genderObj.delete()
+            
+            messages.success(request, 'Gender Deleted Successfully!')
+            return redirect('/gender/list')
+        else:
+            genderObj = Genders.objects.get(pk=genderId)
+            data = {
+                'gender': genderObj
+            }
+            return render(request, 'gender/DeleteGender.html', data)
+    except Exception as e:
+        return HttpResponse(f'Error occured during delete gender {e}')
